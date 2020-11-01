90dc848c48a387ac1fe40751a90c8dea864c9e52
Dejan Stojcevski <dejan.stojcevski@gmail.com>
2020-11-01 19:27:41 +0100

    another commit

--- two_files.py
+++ two_files.py
@@ -3,3 +3,4 @@ for last_line in open(r'C:\Users\Admin001\Desktop\Python\sample1.csv'):pass
 for line in open(r'C:\Users\Admin001\Desktop\Python\sample.csv'): l += line,
 x=l.index(last_line)+1 if last_line in l else 0
 for item in l[x:]: print(item,end='')
+#print(*l[x:],end='')
